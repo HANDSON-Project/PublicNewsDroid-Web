caedce1b4c3774850b20896a0b7e059a208c5571
identity16 <dnjswns0930@gmail.com>
2021-12-02 18:25:07 +0900

    Add: Login & Register Pages

---
+++ src/common/BtnPrimary.jsx
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+const BtnPrimary = styled.button`
+    cursor: pointer;
+    display: block;
+    width: 100%;
+    text-align: center;
+    background-color: #79afff;
+    padding: 12px 0;
+    font-size: 16px;
+    font-weight: bold;
+    line-height: 1;
+    border: none;
+    color: #fff;
+
+    &:hover {
+        background-color: #3d8bff;
+    }
+`;
+
+export default BtnPrimary;
--- src/user/Login.jsx
+++ src/user/Login.jsx
@@ -1,7 +1,74 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
+import Input from "../common/Input";
+import styled from "styled-components";
+import { Link, useNavigate } from "react-router-dom";
+import BtnPrimary from "../common/BtnPrimary";
 
-function Home() {
-    return <div className="Home"></div>;
+const LoginContainer = styled.div`
+    display: flex;
+    height: 100%;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding: 48px;
+
+    .Input__pw {
+        margin-top: 20px;
+    }
+`;
+
+const Title = styled.h1`
+    text-align: center;
+    font-size: 28px;
+    margin: 1.5em;
+`;
+
+const BtnLogin = styled(BtnPrimary)`
+    margin: 24px 0 16px;
+`;
+
+const BtnRegister = styled(Link)`
+    margin: 0 auto;
+    text-align: center;
+    color: #999;
+    font-size: 12px;
+    font-weight: bold;
+`;
+
+function Login() {
+    const navigate = useNavigate();
+    const [id, setId] = useState("");
+    const [pw, setPw] = useState("");
+
+    const onIdChange = useCallback((e) => setId(e.target.value), []);
+    const onPwChange = useCallback((e) => setPw(e.target.value), []);
+    const doLogin = useCallback(() => {
+        console.log("Try login with: ", id, pw);
+
+        navigate("/home");
+    }, [id, pw, navigate]);
+
+    return (
+        <LoginContainer>
+            <Title>로그인</Title>
+            <Input
+                type="text"
+                name="id"
+                value={id}
+                placeholder="아이디"
+                onChange={onIdChange}
+            />
+            <Input
+                type="password"
+                name="pw"
+                value={pw}
+                placeholder="비밀번호"
+                onChange={onPwChange}
+            />
+            <BtnLogin onClick={doLogin}>로그인</BtnLogin>
+            <BtnRegister to="/register">회원가입</BtnRegister>
+        </LoginContainer>
+    );
 }
 
-export default Home;
+export default Login;
