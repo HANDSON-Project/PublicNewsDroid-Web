dbbf1fe1681b499e5631a5bcc058ccdcf5cb4496
identity16 <dnjswns0930@gmail.com>
2021-12-13 16:21:30 +0900

    Feature: Add Comment

--- src/news/CommentList.jsx
+++ src/news/CommentList.jsx
@@ -1,7 +1,7 @@
 import React, { useCallback, useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import styled from "styled-components";
-import { getCommentList } from "../api/news";
+import { deleteComment, getCommentList } from "../api/news";
 import CommentItem from "./CommentItem";
 
 const CommentListContainer = styled.div`
@@ -41,16 +41,66 @@ const CommentListHeader = styled.header`
     }
 `;
 
-const StyledUl = styled.ul``;
+const StyledUl = styled.ul`
+    padding: 0;
+    margin: 0;
+`;
+
+const CommentWriteForm = styled.nav`
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    display: flex;
+    border-top: solid 1px #333;
+`;
+
+const CommentWriteInput = styled.textarea`
+    margin: 12px;
+    width: 100%;
+    background-color: #fafafa;
+    border: solid 1px #f0f0f0;
+    border-radius: 4px;
+`;
+
+const CommentWriteBtn = styled.button`
+    cursor: pointer;
+    border: none;
+    background-color: #5297ff;
+    color: #fff;
+    font-size: 14px;
+    font-weight: bold;
+    width: 88px;
+    height: 136px;
+`;
 
 function CommentList({ user }) {
     const { id } = useParams();
     const navigate = useNavigate();
     const [commentList, setCommentList] = useState([]);
+    const [newComment, setNewComment] = useState("");
     const onBackClicked = useCallback(
         () => navigate(`/news/${id}`),
         [navigate, id]
     );
+
+    const onCommentItemDelete = useCallback(
+        (commentId) => {
+            if (window.confirm("정말 삭제하시겠습니까?")) {
+                deleteComment(commentId).then(
+                    (result) =>
+                        result &&
+                        getCommentList(id).then((comments) =>
+                            setCommentList(comments)
+                        )
+                );
+            }
+        },
+        [id]
+    );
+
+    const onCommentWrite = useCallback(() => {
+        console.log(newComment);
+    }, [newComment]);
 
     useEffect(() => {
         if (user === null) {
@@ -72,9 +122,18 @@ function CommentList({ user }) {
                     <CommentItem
                         comment={comment}
                         isMe={user && user.id === comment.userId}
+                        onDelete={onCommentItemDelete}
                     />
                 ))}
             </StyledUl>
+            <CommentWriteForm>
+                <CommentWriteInput
+                    placeholder="댓글을 입력해주세요.."
+                    value={newComment}
+                    onChange={(e) => setNewComment(e.target.value)}
+                ></CommentWriteInput>
+                <CommentWriteBtn onClick={onCommentWrite}>작성</CommentWriteBtn>
+            </CommentWriteForm>
         </CommentListContainer>
     );
 }
