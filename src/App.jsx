06c672cf1f8db645357a2d7df3bbd9c57999ca4b
identity16 <dnjswns0930@gmail.com>
2021-12-17 16:25:31 +0900

    API: Login, Register

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,44 @@
 import "./App.css";
-import React, { useState } from "react";
+import React, { useCallback, useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NewsList from "./news/NewsList";
 import Login from "./user/Login";
 import Register from "./user/Register";
-import dummyUser from "./data/dummy_user.json";
 import NewsWrite from "./news/NewsWrite";
 import NewsDetail from "./news/NewsDetail";
 import CommentList from "./news/CommentList";
+import { getUser, login, register } from "./api/user";
 
 function App() {
     const [user, setUser] = useState(null);
+    const [jwt, setJwt] = useState("");
 
-    const loginAction = async () => {
-        setUser(dummyUser);
-        return true;
-    };
+    const loginAction = useCallback(async (id, pw) => {
+        const result = await login(id, pw);
+        if (!result) return null;
+
+        const data = await getUser(result.jwt, result.userIdx);
+
+        setJwt(result.jwt);
+        setUser(data);
+
+        return data;
+    }, []);
+
+    const registerAction = useCallback(
+        async (email, pw, nickname, location) => {
+            const result = await register(email, pw, nickname, location);
+            if (!result) return null;
+
+            const data = await getUser(result.jwt, result.userIdx);
+
+            setJwt(result.jwt);
+            setUser(data);
+
+            return data;
+        },
+        []
+    );
 
     return (
         <div className="App">
@@ -23,20 +46,26 @@ function App() {
                 <Routes>
                     <Route path="/" exact element={<NewsList user={user} />} />
                     <Route path="/home" element={<NewsList user={user} />} />
-                    <Route path="/write" element={<NewsWrite user={user} />} />
+                    <Route
+                        path="/write"
+                        element={<NewsWrite jwt={jwt} user={user} />}
+                    />
                     <Route
                         path="/news/:id"
-                        element={<NewsDetail user={user} />}
+                        element={<NewsDetail jwt={jwt} user={user} />}
                     />
                     <Route
                         path="/news/:id/comment"
-                        element={<CommentList user={user} />}
+                        element={<CommentList jwt={jwt} user={user} />}
                     />
                     <Route
                         path="/login"
                         element={<Login action={loginAction} />}
                     />
-                    <Route path="/register" element={<Register />} />
+                    <Route
+                        path="/register"
+                        element={<Register action={registerAction} />}
+                    />
                 </Routes>
             </Router>
         </div>
